8a8a3bd87d3a53dbd0b1de22f8ea7cc3e37e61f0
Nicolas Dandrimont <nicolas@dandrimont.eu>
2021-12-28 15:57:04 +0100

    Revert "Make password and secure_json_data in grafana_datasource sensitive so passwords aren't leaked"
    This reverts commit b127d6229678f542d98364db1dc79cc3232fefe0.
    
    This is incompatible with puppet 5

--- lib/puppet/type/grafana_datasource.rb
+++ lib/puppet/type/grafana_datasource.rb
@@ -56,7 +56,6 @@ Puppet::Type.newtype(:grafana_datasource) do
 
   newproperty(:password) do
     desc 'The password for the datasource (optional)'
-    sensitive true
   end
 
   newproperty(:database) do
@@ -107,7 +106,6 @@ Puppet::Type.newtype(:grafana_datasource) do
 
   newproperty(:secure_json_data) do
     desc 'Additional secure JSON data to configure the datasource (optional)'
-    sensitive true
 
     validate do |value|
       raise ArgumentError, 'secure_json_data should be a Hash!' unless value.nil? || value.is_a?(Hash)
